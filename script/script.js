f066839fed37c6e9f61593c2594338663a42760a
aishmam1999 <54598961+aishmam1999@users.noreply.github.com>
2019-11-26 21:59:47 -0600

    Add files via upload

--- script/script.js
+++ script/script.js
@@ -1,13 +1,11 @@
-
-
 // Initialize and add the map
 function initMap() {
   // The location of Uluru
-  var uluru = {lat: 41.833298 , lng:  -87.6237};
+  var uluru = {lat: 41.834873, lng: -87.627007};
   // The map, centered at Uluru
   var map = new google.maps.Map(
-      document.getElementById('map'), {zoom: 4, center: uluru});
+      document.getElementById('map'), {zoom: 15, center: uluru});
   // The marker, positioned at Uluru
   var marker = new google.maps.Marker({position: uluru, map: map});
 }
-   
+  
